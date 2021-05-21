67bba51608be9766d6694b569ce4bc2847045945
jbburk <jbburk@byui.edu>
2021-05-21 13:01:19 -0500

    mostly finished jumper program

--- jumper/game/word.py
+++ jumper/game/word.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Word:
     """The responsibility of this class of objects is to get a word, 
     encrypt the word, and revel correct letters guessed
@@ -9,10 +10,51 @@ class Word:
     Attributes:
     """
     def __init__(self):
-        self.word
+        self.word = []
+        self.hidden = []
+        self.wrong_guesses = 0
+        self.wrong_list =[]
+        self.wordList = []
+        with open("words.txt","r") as word_file:
+            word_file_lines = word_file.readlines()
+            for line in word_file_lines:
+                line = line.strip("\n")
+                self.wordList.append()
 
 
+    def get_word(self):
+        self.word = list(random.choice(self.wordList))
+        
+    def hide_word(self):
+        for char in self.word:
+            self.hidden.append("_ ")
 
+    def check_guess(self, guess):
+        if guess not in self.hidden:
+            if guess in self.word:
+                for i in range(0, len(self.word)):
+                    if self.word[i] == guess:
+                        self.hidden[i] = guess
+                return "you got it"
+            else:
+                if guess in self.wrong_list:
+                    return "You already guessed that"
+                else:
+                    self.wrong_guesses += 1
+                    self.wrong_list.append(guess)
+                    return "you suck"
+
+        else:
+            return "You've already guessed that"
+
+    def check_game(self):
+        if self.wrong_guesses == 5:
+            print("Sorry, you suck")
+            return "Loss"
+        if "_ " not in self.hidden:
+            print("Congrats, you got it")
+            return "Win"
+        
     """Code taken from Solo Checkpoint Rabbit.py to use as a reference
     def __init__(self):
         self.location = random.randint(1, 1000)
